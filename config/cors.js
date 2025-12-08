ad16c5bfda09efc4ab8806725535be0758ccb1fc
basteen-Dev <basteen@digitfellas.com>
2025-12-08 16:23:18 +0530

    updated

--- config/cors.js
+++ config/cors.js
@@ -6,7 +6,7 @@ const DEFAULT_ORIGINS = [
   'http://127.0.0.1:5174',
   'https://snowcity.netlify.app',
   'https://snowpark.netlify.app',
-  'https://qa.phicommerce.com/pg'
+  'https://qa.phicommerce.com'
 ];
 
 const parseOrigins = () => {
